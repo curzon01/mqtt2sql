6e603e0def31d49d878e9dc5cf5c9f1700eb6618
Norbert Richter <norbert.richter@p-r-solution.de>
2017-04-10 14:46:05 +0200

    v1.1.0006 - src clean-up - program parameter argument grouped - parm for sql table name added - parm for mqtt topic enhanced to allow multiple topics

--- README.md
+++ README.md
@@ -2,13 +2,11 @@
 This python script subscribes to MQTT broker topic and inserts the topic into a mysql database table
 
 ## Installation
-* Copy repository using git:
+### Prerequisite
+* Paho MQTT and MySQLdb python lib are neccessary. To install it to you python environment use
 ```
-git clone https://github.com/curzon01/mqtt2mysql
-```
-* Make it executable:
-```
-chmod +x mqtt2mysql/mqtt2mysql.py
+sudo apt-get install python-pip python-mysqldb
+sudo pip install paho-mqtt argparse
 ```
 * Create database table using SQL command
 ```
@@ -23,9 +21,41 @@ CREATE TABLE `mqtt` (
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
 ```
+### Copy the program
+* Copy repository using git:
+```
+git clone https://github.com/curzon01/mqtt2mysql
+```
+* Make it executable:
+```
+chmod +x mqtt2mysql/mqtt2mysql.py
+```
 
 ## Usage
-For help start the script with parameter -h
+For first help, start the script with parameter -h
 ```
 mqtt2mysql/mqtt2mysql.py -h
+```
+### Systemd manager daemon example
+Create mqtt2mysql.service
+```
+sudo nano /etc/systemd/system/mqtt2mysql.service
+```
+Insert the following - replace example usernames and passwords with yours
+```
+Description=MQTT2MySQL
+After=local-fs.target network.target mysql.service
+ 
+[Service]
+Type=simple
+Restart=always
+RestartSec=10
+ExecStart=/usr/local/bin/mqtt2mysql.py --host phmqtt.project-home.local --username mqttuser --password mqttpasswd --topic myhome/# --sql_username mysqluser --sql_password 'mysqlpasswd' --sql_db mydb --logfile /var/log/mqtt.log
+
+[Install]
+WantedBy=multi-user.target
+```
+Finally reload systemd manager
+```
+sudo systemctl daemon-reload
 ```
--- mqtt2mysql.py
+++ mqtt2mysql.py
@@ -8,17 +8,16 @@
 #              into a mysql database table
 # Author:      Norbert Richter <norbert.richter@p-r-solution.de>
 #
-# Parameter:   -
+# Parameter:   
 
 import paho.mqtt.client as mqtt
 import MySQLdb
-import json
 import os, sys
 import time
 import signal
 import argparse
 
-VERSION = '1.0.0005'
+VERSION = '1.1.0006'
 
 args = {}
 
@@ -37,7 +36,10 @@ def on_connect(client, userdata, message):
 
 	if args.debug>0:
 		log("on_connect message %s" % str(message))
-	client.subscribe(args.mqtt_topic, 0)
+	for topic in args.mqtt_topic:
+		if args.debug>0:
+			log("subscribing to topic %s" % topic)
+		client.subscribe(topic, 0)
 
 
 def on_message(client, userdata, message):
@@ -49,10 +51,10 @@ def on_message(client, userdata, message):
 
 		try:
 			# INSERT or UPDATE
-			cursor.execute("INSERT INTO mqtt SET topic='%s', value='%s' ON DUPLICATE KEY UPDATE value='%s'" % (message.topic, str(message.payload), str(message.payload)))
+			cursor.execute("INSERT INTO `%s` SET `topic`='%s', `value`='%s' ON DUPLICATE KEY UPDATE `value`='%s'" % (args.sql_table, message.topic, str(message.payload), str(message.payload)))
 			db.commit()
 			if args.debug>0:
-				log("SQL successful written: topic='%s', value'%s'" % (message.topic, str(message.payload)))
+				log("SQL successful written: table='%s', topic='%s', value'%s'" % (args.sql_table, message.topic, str(message.payload)))
 
 		except MySQLdb.Error, e:
 			try:
@@ -96,24 +98,32 @@ def signal_term_handler(signal, frame):
 signal.signal(signal.SIGTERM, signal_term_handler)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--host', metavar="<mqtt_host>", help="MQTT host to connect to (defaults to localhost)", dest='mqtt_host', default="localhost")
-parser.add_argument('--port', metavar="<mqtt_port>", help="MQTT network port to connect to (defaults to 1883)", dest='mqtt_port', default=1883, type=int)
-parser.add_argument('--username', metavar="<mqtt_username>", help="MQTT username (defaults to None)", dest='mqtt_username', default=None)
-parser.add_argument('--password', metavar="<mqtt_password>", help="MQTT password (defaults to None)", dest='mqtt_password', default=None)
-parser.add_argument('--topic', metavar="<topic>", help="MQTT topic to use (defaults to #)", dest='mqtt_topic', default='#')
-parser.add_argument('--cafile', metavar="<cafile>", help="MQTT cafile (defaults to None)", dest='mqtt_cafile', default=None)
-parser.add_argument('--certfile', metavar="<certfile>", help="MQTT certfile (defaults to None)", dest='mqtt_certfile', default=None)
-parser.add_argument('--keyfile', metavar="<keyfile>", help="MQTT keyfile (defaults to None)", dest='mqtt_keyfile', default=None)
-parser.add_argument('--insecure', help="suppress TLS verification of MQTT hostname", dest='mqtt_insecure', default=False, action='store_true')
-parser.add_argument('--sql_host', metavar="<sql_host>", help="SQL host to connect to (defaults to localhost)", dest='sql_host', default="localhost")
-parser.add_argument('--sql_port', metavar="<sql_port>", help="SQL network port to connect to (defaults to 3306)", dest='sql_port', default=3306, type=int)
-parser.add_argument('--sql_username', metavar="<sql_username>", help="SQL username (defaults to None)", dest='sql_username', default=None)
-parser.add_argument('--sql_password', metavar="<sql_password>", help="SQL password (defaults to None)", dest='sql_password', default=None)
-parser.add_argument('--sql_db', metavar="<sql_db>", help="SQL database to use (defaults to None)", dest='sql_db', default=None)
+mqtt_group = parser.add_argument_group('MQTT Options')
+mqtt_group.add_argument('--host', metavar="<mqtt_host>", help="MQTT host to connect to (defaults to localhost)", dest='mqtt_host', default="localhost")
+mqtt_group.add_argument('--port', metavar="<mqtt_port>", help="MQTT network port to connect to (defaults to 1883)", dest='mqtt_port', default=1883, type=int)
+mqtt_group.add_argument('--username', metavar="<mqtt_username>", help="MQTT username (defaults to None)", dest='mqtt_username', default=None)
+mqtt_group.add_argument('--password', metavar="<mqtt_password>", help="MQTT password (defaults to None)", dest='mqtt_password', default=None)
+mqtt_group.add_argument('--topic', metavar="<topic>", help="MQTT topic to use (defaults to #)", dest='mqtt_topic', default='#', nargs='*')
+mqtt_group.add_argument('--cafile', metavar="<cafile>", help="MQTT cafile (defaults to None)", dest='mqtt_cafile', default=None)
+mqtt_group.add_argument('--certfile', metavar="<certfile>", help="MQTT certfile (defaults to None)", dest='mqtt_certfile', default=None)
+mqtt_group.add_argument('--keyfile', metavar="<keyfile>", help="MQTT keyfile (defaults to None)", dest='mqtt_keyfile', default=None)
+mqtt_group.add_argument('--insecure', help="suppress TLS verification of MQTT hostname", dest='mqtt_insecure', default=False, action='store_true')
 
-parser.add_argument('-l', '--logfile', metavar="<logfile>", help="additional logfile", dest='logfile', default=None)
-parser.add_argument('-d', '--debug', help="debug output", dest='debug', action='count')
-parser.add_argument('-v', '--verbose', help="verbose output", dest='verbose', action='count')
+sql_group = parser.add_argument_group('SQL Options')
+sql_group.add_argument('--sql_host', metavar="<sql_host>", help="SQL host to connect to (defaults to localhost)", dest='sql_host', default="localhost")
+sql_group.add_argument('--sql_port', metavar="<sql_port>", help="SQL network port to connect to (defaults to 3306)", dest='sql_port', default=3306, type=int)
+sql_group.add_argument('--sql_username', metavar="<sql_username>", help="SQL username (defaults to None)", dest='sql_username', default=None)
+sql_group.add_argument('--sql_password', metavar="<sql_password>", help="SQL password (defaults to None)", dest='sql_password', default=None)
+sql_group.add_argument('--sql_db', metavar="<sql_db>", help="SQL database to use (defaults to None)", dest='sql_db', default=None)
+sql_group.add_argument('--sql_table', metavar="<sql_table>", help="SQL database table to use (defaults to mqtt)", dest='sql_table', default="mqtt")
+
+logging_group = parser.add_argument_group('Logging')
+logging_group.add_argument('-l', '--logfile', metavar="<logfile>", help="additional logfile", dest='logfile', default=None)
+logging_group.add_argument('-v', '--verbose', help="verbose output", dest='verbose', action='count')
+
+debug_group = parser.add_argument_group('Debug Options')
+debug_group.add_argument('-d', '--debug', help="debug output", dest='debug', action='count')
+
 args = parser.parse_args()
 
 scriptname = os.path.basename(sys.argv[0])
@@ -123,6 +133,8 @@ log('%s v%s start' % (scriptname, VERSION))
 mqttc = mqtt.Client(scriptname)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
 mqttc.on_log = on_log
 
 # cafile controls TLS usage
